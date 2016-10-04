b4bf1f600c85aca635f55286509336ba36b3749f
putridfoetus <putridfoetus@gmail.com>
2016-10-04 08:47:36 -0500

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -2,3 +2,4 @@
 
 # This script will be executed in post-fs-data mode
 # More info in the main Magisk thread
+/magisk/.core/bin/sepolicy-inject --live -s mediaserver -t mediaserver_tmpfs -c file -p read,write,execute
